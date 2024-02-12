b5f506dc7d28df989110cd6ad55dbe836cab3c4e
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2024-02-12 15:55:35 -0500

    Updates

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -1,16 +1,7 @@
 import React from "react";
-import StepOne from "./steps/StepOne";
-import NextStep from "./NextStep";
-import Pagination from "./Pagination";
 
 function App() {
-  return (
-    <div style={{ backgroundColor: "#D6D9E6", position: "relative" }}>
-      <Pagination />
-      <StepOne />
-      <NextStep />
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
--- client/app/src/NextStep.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function NextStep() {
-  return <div>NextStep</div>;
-}
-
-export default NextStep;
